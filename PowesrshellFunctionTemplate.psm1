accc6985c4153d9071fb35584b07f2e3433c5caa
heidarj <heidarj@nyherji.is>
2015-04-12 11:21:10 +0000

    Powesrshell Function Template

---
+++ PowesrshellFunctionTemplate.psm1
@@ -0,0 +1,13 @@
+function template {
+    begin {
+        #runs once before main function
+    }
+    
+    process {
+        #runs once for each itme in the pipline
+    }
+    
+    end {
+        #runs once after main function
+    }
+}
